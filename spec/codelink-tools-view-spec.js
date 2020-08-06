0c958c5cc1dbb151dd6cc1e3817347d483417646
j.woudenberg <j.woudenberg@linkeng.com>
2020-08-06 09:15:00 -0400

    Initial commit of codelink-tools atom package

---
+++ spec/codelink-tools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodelinkToolsView from '../lib/codelink-tools-view';
+
+describe('CodelinkToolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
