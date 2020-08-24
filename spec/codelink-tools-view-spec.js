4937ed79499790e0da36e1606a92954d3b2fbe43
j.woudenberg <j.woudenberg@linkeng.com>
2020-08-24 14:37:21 -0400

    Parser lists results, which are clickable and highlighted

--- spec/codelink-tools-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CodelinkToolsView from '../lib/codelink-tools-view';
-
-describe('CodelinkToolsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
